803087d397d0ead7e4f9ba853e255f9346ad228b
Mervine, Jonathan <jon.mervine@gmail.com>
2019-09-30 10:45:35 -0400

    Finished up NutFarm2 using a recursive "action" function to figure out multiple moves for a single nut. Theres probably a lot of room for optimization but for a simple solution it gets the job done with little change to the first NutFarm

--- codewars/src/main/java/com/darkmage530/codewars/kyu5/NutFarm2.java
+++ codewars/src/main/java/com/darkmage530/codewars/kyu5/NutFarm2.java
@@ -1,7 +1,5 @@
 package com.darkmage530.codewars.kyu5;
 
-import com.darkmage530.codewars.kyu6.NutFarm;
-
 public class NutFarm2 {
 
     private static final char NUT = 'o', // a nut
@@ -14,37 +12,32 @@ public class NutFarm2 {
 
     public static void main(String[] args) {
         char[][] tree = {
-                {'.','o','.','o','o','o','o','o','o','.','o','.','o','.','o','o','o','o','o','o','.'},
-                {'.','.','\\','.','\\','.','.','.','/','.','.','\\','.','.','.','/','.','.','\\','.','.'}
+                {'.', 'o', '.', 'o', 'o', 'o', 'o', 'o', 'o', '.', 'o', '.', 'o', '.', 'o', 'o', 'o', 'o', 'o', 'o', '.'},
+                {'.', '.', '\\', '.', '\\', '.', '.', '.', '/', '.', '.', '\\', '.', '.', '.', '/', '.', '.', '\\', '.', '.'}
         };
 
         int[] finalNuts = NutFarm2.shakeTree(tree);
 
-        int[] solution = new int[] {0,1,0,1,0,2,1,2,0,0,1,0,1,0,2,0,1,1,0,2,0};
+        int[] solution = new int[]{0, 1, 0, 1, 0, 2, 1, 2, 0, 0, 1, 0, 1, 0, 2, 0, 1, 1, 0, 2, 0};
     }
-
-//    public static int[] shakeTree(final char[][] tree) {
-//        // Your code here
-//        return new int[tree[0].length];
-//    }
 
     public static int[] shakeTree(final char[][] tree) {
         NutFarm2 nutFarm = new NutFarm2();
 
         int treeHeight = tree.length;
         int treeWidth = tree[0].length;
-        int[] nutLocation = nutFarm.getNutLocations(tree[0]);
+        int[] nutLocation = new int[treeWidth];
 
         for (int row = 0; row < treeHeight; row++) {
-            if (row < (treeHeight-1)) { //Assume we aren't on the bottom
-                char[] nextRow = tree[row+1];
+            if (row < (treeHeight - 1)) { //Assume we aren't on the bottom
+                nutLocation = nutFarm.findNewNuts(tree[row], nutLocation);
+                char[] nextRow = tree[row + 1];
                 for (int column = 0; column < treeWidth; column++) {
                     if (nutLocation[column] > 0) { //we found a nut(s)
                         nutLocation = nutFarm.nutAction(nutLocation, nextRow, column);
                     }
                 }
-            }
-            else {
+            } else {
                 //assume we are on the bottom so don't do anything
             }
         }
@@ -52,12 +45,22 @@ public class NutFarm2 {
         return nutLocation;
     }
 
+    /**
+     * Uses Recursion to determine how far a nut may bounce if there are multiple branches next to each other
+     * The base cases are that a nut then reaches an area where it will continue falling or gets stuck
+     *
+     * @param nutLocation
+     * @param belowNuts
+     * @param columnIndex
+     * @return
+     */
     int[] nutAction(int[] nutLocation, char[] belowNuts, int columnIndex) {
         char belowNut = belowNuts[columnIndex];
         if ((belowNut == RIGHT_BRANCH &&
-                belowNuts[columnIndex+1] == LEFT_BRANCH)
+                belowNuts[columnIndex + 1] == LEFT_BRANCH)
                 || (belowNut == LEFT_BRANCH &&
-                belowNuts[columnIndex-1] == RIGHT_BRANCH)) {
+                belowNuts[columnIndex - 1] == RIGHT_BRANCH)) {
+            //A nut will bounce back and forth indefinitely between \ / branches so treat it as a Stuck Nut
             nutStuck(columnIndex, nutLocation);
         } else if (belowNut == RIGHT_BRANCH) {
             nutLocation = moveNutRight(columnIndex, nutLocation);
@@ -67,27 +70,34 @@ public class NutFarm2 {
             nutLocation = nutAction(nutLocation, belowNuts, --columnIndex);
         } else if (belowNut == STUCK_BRANCH) {
             nutLocation = nutStuck(columnIndex, nutLocation);
-
         }
         return nutLocation;
     }
 
     private int[] moveNutLeft(int column, int[] nutLocation) {
         int nutsInSpot = nutLocation[column];
-        nutLocation[column-1] += nutsInSpot;
+        nutLocation[column - 1] += nutsInSpot;
         nutLocation[column] = 0;
         return nutLocation;
     }
 
     private int[] moveNutRight(int column, int[] nutLocation) {
         int nutsInSpot = nutLocation[column];
-        nutLocation[column+1] += nutsInSpot;
+        nutLocation[column + 1] += nutsInSpot;
         nutLocation[column] = 0;
         return nutLocation;
     }
 
     private int[] nutStuck(int column, int[] nutLocation) {
         nutLocation[column] = 0;
+        return nutLocation;
+    }
+
+    private int[] findNewNuts(char[] currentTreeLevel, int[] nutLocation) {
+        int[] newNutLocations = getNutLocations(currentTreeLevel);
+        for (int i = 0; i < newNutLocations.length; i++) {
+            nutLocation[i] = nutLocation[i] + newNutLocations[i];
+        }
         return nutLocation;
     }
 
