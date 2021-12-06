eb382db02f99eea7c83fa3c9a2d7e4244db6587a
Mervine, Jonathan <jon.mervine@gmail.com>
2021-12-06 13:38:21 -0500

    Just making sure i can commit and all that correctly on work machine

---
+++ advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/y2021/Day6.kt
@@ -0,0 +1,13 @@
+package com.darkmage530.adventOfCode.y2021
+
+import com.darkmage530.adventOfCode.getTestInputLines
+
+class Day6 {
+}
+
+fun main() {
+    val testData = getTestInputLines(testInput)
+    Day6()
+}
+
+private const val testInput = ""
