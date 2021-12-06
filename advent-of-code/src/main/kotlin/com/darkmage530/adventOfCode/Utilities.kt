ddad725e7635b49d8d4bcd6de67f630d7e73fc19
Mervine, Jonathan <jon.mervine@gmail.com>
2021-12-06 17:10:40 -0500

    Added some single line parsing utility functions Solved Day6 problem

--- advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/Utilities.kt
+++ advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/Utilities.kt
@@ -5,3 +5,7 @@ import java.io.File
 fun getTestInputLines(testInput: String) = run { testInput.split("\n") }
 
 fun getTestFileLines(filename: String) = run { File("advent-of-code/src/main/resources/2021/$filename").readLines()}
+
+fun getSingleTestInputLines(inputString: String, delimiter: String) = run { inputString.split(delimiter)}
+
+fun getSingleTestFileLine(filename: String, delimiter: String) = run {File("advent-of-code/src/main/resources/2021/$filename").readLines().first().split(delimiter)}
--- advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/y2021/Day6.kt
+++ advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/y2021/Day6.kt
@@ -1,13 +1,58 @@
 package com.darkmage530.adventOfCode.y2021
 
-import com.darkmage530.adventOfCode.getTestInputLines
+import com.darkmage530.adventOfCode.getSingleTestFileLine
+import com.darkmage530.adventOfCode.getSingleTestInputLines
+import java.math.BigInteger
 
 class Day6 {
+    fun part1(testData: List<String>) {
+        var radixBasedTable: MutableList<Int> = MutableList(9){0}
+        for(fish in testData) {
+            radixBasedTable[Integer.parseInt(fish)]++
+        }
+
+        var altRadixTable: MutableList<Int> = MutableList(9){0}
+        for (i in 1..80) {
+            for (y in 1..8) {
+                altRadixTable[y-1] = radixBasedTable[y]
+            }
+            val spawningFish = radixBasedTable[0]
+            altRadixTable[6] += spawningFish
+            altRadixTable[8] = spawningFish
+            radixBasedTable = altRadixTable.toMutableList()
+            altRadixTable = MutableList(9){0}
+        }
+        val totalFish = radixBasedTable.sum()
+        println("totalFish: $totalFish")
+    }
+
+    fun part2(testData: List<String>) {
+        var radixBasedTable: MutableList<BigInteger> = MutableList(9){ BigInteger.ZERO}
+        for(fish in testData) {
+            radixBasedTable[Integer.parseInt(fish)]++
+        }
+
+        var altRadixTable: MutableList<BigInteger> = MutableList(9){ BigInteger.ZERO}
+        for (i in 1..256) {
+            for (y in 1..8) {
+                altRadixTable[y-1] = radixBasedTable[y]
+            }
+            val spawningFish = radixBasedTable[0]
+            altRadixTable[6] += spawningFish
+            altRadixTable[8] = spawningFish
+            radixBasedTable = altRadixTable.toMutableList()
+            altRadixTable = MutableList(9){ BigInteger.ZERO}
+        }
+        val totalFish = radixBasedTable.sumOf { it }
+        println("totalFish: $totalFish")
+    }
 }
 
 fun main() {
-    val testData = getTestInputLines(testInput)
-    Day6()
+//    val testData = getSingleTestInputLines(testInput, ",")
+    val testData = getSingleTestFileLine("Day6.txt", ",")
+    Day6().part1(testData)
+    Day6().part2(testData)
 }
 
-private const val testInput = ""
+private const val testInput = "3,4,3,1,2"
