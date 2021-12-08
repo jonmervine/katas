20938038e2c4ec63baf8bd7892891cc4353a54cc
Mervine, Jonathan <jon.mervine@gmail.com>
2021-12-07 23:11:07 -0500

    Added day 7 and i struggled on this for some reason

--- advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/y2021/Day6.kt
+++ advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/y2021/Day6.kt
@@ -27,25 +27,47 @@ class Day6 {
     }
 
     fun part2(testData: List<String>) {
-        var radixBasedTable: MutableList<BigInteger> = MutableList(9){ BigInteger.ZERO}
+        var fishTimers: MutableList<BigInteger> = MutableList(9){ BigInteger.ZERO}
         for(fish in testData) {
-            radixBasedTable[Integer.parseInt(fish)]++
+            fishTimers[Integer.parseInt(fish)]++
         }
 
-        var altRadixTable: MutableList<BigInteger> = MutableList(9){ BigInteger.ZERO}
+        var tempHolderFishTable: MutableList<BigInteger> = MutableList(9){ BigInteger.ZERO}
         for (i in 1..256) {
             for (y in 1..8) {
-                altRadixTable[y-1] = radixBasedTable[y]
+                tempHolderFishTable[y-1] = fishTimers[y]
             }
-            val spawningFish = radixBasedTable[0]
-            altRadixTable[6] += spawningFish
-            altRadixTable[8] = spawningFish
-            radixBasedTable = altRadixTable.toMutableList()
-            altRadixTable = MutableList(9){ BigInteger.ZERO}
+            val spawningFish = fishTimers[0]
+            tempHolderFishTable[6] += spawningFish
+            tempHolderFishTable[8] = spawningFish
+            fishTimers = tempHolderFishTable.toMutableList()
+            tempHolderFishTable = MutableList(9){ BigInteger.ZERO}
         }
-        val totalFish = radixBasedTable.sumOf { it }
+        val totalFish = fishTimers.sumOf { it }
         println("totalFish: $totalFish")
     }
+
+    fun altPart2(testData: List<String>) {
+        var fishTimers: MutableList<BigInteger> = MutableList(9){ BigInteger.ZERO}
+        for(fish in testData) {
+            fishTimers[Integer.parseInt(fish)]++
+        }
+
+        var spawningIndex = 0
+        for (i in 1..256) {
+            if (spawningIndex == 7) spawningIndex = 0
+            val fishToSpawn = fishTimers[spawningIndex]
+            fishTimers[(spawningIndex + 7) % 7] += fishTimers[7]
+            fishTimers[7] = fishTimers[8]
+            fishTimers[8] = fishToSpawn
+
+            spawningIndex++
+        }
+        val totalFish = fishTimers.sumOf { it }
+        println("totalFish: $totalFish")
+    }
+
+    private data class FishTimer(var numberOfFish: BigInteger, var next: FishTimer?, var prev: FishTimer?)
 }
 
 fun main() {
@@ -53,6 +75,7 @@ fun main() {
     val testData = getSingleTestFileLine("Day6.txt", ",")
     Day6().part1(testData)
     Day6().part2(testData)
+    Day6().altPart2(testData)
 }
 
 private const val testInput = "3,4,3,1,2"
---
+++ advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/y2021/Day7.kt
@@ -0,0 +1,64 @@
+package com.darkmage530.adventOfCode.y2021
+
+import com.darkmage530.adventOfCode.getSingleTestFileLine
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
+
+class Day7 {
+    fun getPivot(sorted: List<Int>): Int {
+        return if (sorted.size %2 == 1) sorted[(sorted.size - 1) / 2] else ((sorted[(sorted.size/2)-1] + sorted[sorted.size/2])/2.0).roundToInt()
+    }
+
+    fun calculateFuel(sorted: List<Int>, pivot: Int, calculationFormula: (Int, Int) -> Int): Int {
+        var countFuel = 0
+        sorted.forEach { countFuel += calculationFormula(pivot, it) }
+        return countFuel
+    }
+
+    val simpleIteratingFunction: (Int, Int) -> Int = { pivot: Int, n: Int -> (pivot - n).absoluteValue }
+    val complexIteratingFunction: (Int, Int) -> Int = { pivot: Int, iter: Int ->  val n = (pivot - iter).absoluteValue
+        ((n * n + n) / 2) }
+
+    fun part1(testData: List<String>) {
+        val sorted = testData.map { it.toInt() }.sorted()
+        var pivot = getPivot(sorted)
+
+        val countFuel = calculateFuel(sorted, pivot, simpleIteratingFunction)
+
+        println(countFuel)
+    }
+
+    fun part2(testData: List<String>) {
+        val sorted = testData.map { it.toInt() }.sorted()
+        var pivot = getPivot(sorted)
+
+        val pivotPlus1 = pivot + 1
+        val pivotMinus1 = pivot - 1
+
+        val countFuelPlus = calculateFuel(sorted, pivotPlus1, complexIteratingFunction)
+        val countFuel = calculateFuel(sorted, pivot, complexIteratingFunction)
+        val countFuelMinus = calculateFuel(sorted, pivotMinus1, complexIteratingFunction)
+
+//        println("pivot $pivot plus1: $countFuelPlus pivot $countFuel minus1 $countFuelMinus")
+
+        var mostLeastFuel = countFuel
+        var direction = if (countFuelMinus < countFuelPlus) -1 else 1
+        println(direction)
+
+        while (calculateFuel(sorted, pivot + direction, complexIteratingFunction) < mostLeastFuel) {
+            mostLeastFuel = calculateFuel(sorted, pivot + direction, complexIteratingFunction)
+            pivot += direction
+//            println("$mostLeastFuel and new pivot $pivot")
+        }
+        println(mostLeastFuel)
+    }
+}
+
+fun main() {
+//    val testData = getSingleTestInputLines(testInput, ",")
+    val testData = getSingleTestFileLine("Day7.txt", ",")
+    Day7().part1(testData)
+    Day7().part2(testData)
+}
+
+private const val testInput: String = "16,1,2,0,4,2,7,1,2,14"
