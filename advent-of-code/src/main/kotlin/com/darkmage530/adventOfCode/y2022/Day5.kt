c73db056e1da25e0428ec86b6e55bc5bc8d668b0
Mervine, Jonathan <jon.mervine@gmail.com>
2022-12-05 12:12:11 -0500

    Starting Day 5

---
+++ advent-of-code/src/main/kotlin/com/darkmage530/adventOfCode/y2022/Day5.kt
@@ -0,0 +1,21 @@
+package com.darkmage530.adventOfCode.y2022
+
+import com.darkmage530.adventOfCode.AocBase
+import com.darkmage530.adventOfCode.getTestFileLines
+
+class Day5 : AocBase {
+    override fun part1(input: List<String>): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>): String {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main() {
+    Day5().apply {
+        println(part1(getTestFileLines(testFile())))
+        println(part2(getTestFileLines(testFile())))
+    }
+}
---
+++ advent-of-code/src/test/kotlin/com/darkmage530/adventOfCode/y2022/Day5Test.kt
@@ -0,0 +1,28 @@
+package com.darkmage530.adventOfCode.y2022
+
+import com.darkmage530.adventOfCode.getTestInputLines
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class Day5Test : StringSpec({
+    val day5 = Day5()
+    val testInput = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+
+    "part1" {
+        day5.part1(getTestInputLines(testInput))
+            .shouldBe("CMZ")
+    }
+
+    "part2" {
+        day5.part2(getTestInputLines(testInput))
+            .shouldBe("12")
+    }
+})
